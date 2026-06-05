a495e016faba51c22f92674881e8ffd11f7ad105
akshay <akshaygolange383@email.com>
2026-06-05 13:16:36 +0530

    env values changed

--- backend/app.js
+++ backend/app.js
@@ -3,10 +3,15 @@ const cors =require("cors")
 
 const app =express()
 
-app.use(cors({
-  origin: process.env.CLIENT_URL,
-  credentials: true
-}));
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://foodie-hub-dun.vercel.app",
+    ],
+    credentials: true,
+  })
+);
 app.use(express.json())
 
 app.get("/",(req,res) =>{
